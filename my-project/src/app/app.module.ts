5803934c9fed1771767510dc0cf67adbeb58e6e3
prisunka <prisunka@cisco.com>
2019-04-26 11:27:34 -0700

    name module created

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -4,10 +4,12 @@ import{ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
