0b280957474f9c07d10ec812008e7ef5423cb76d
prisunka <prisunka@cisco.com>
2019-04-26 12:09:51 -0700

    address module created

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameComponent } from './name/name.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameComponent
+    NameComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
