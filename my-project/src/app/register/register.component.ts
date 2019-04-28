d4f1a9b30288fbff65b32012d1ee4dce93bc48bc
prisunka <prisunka@cisco.com>
2019-04-28 00:04:25 -0700

    custom validator added to confirm password field

--- my-project/src/app/register/register.component.ts
+++ my-project/src/app/register/register.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
   registerForm: FormGroup;
   submitted: boolean = false;
   loading: boolean = false;
@@ -15,11 +16,16 @@ export class RegisterComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder
   ) { }
-   
+
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      firstName: ['', Validators.required]
-    }); 
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: this.MustMatch('password', 'confirmPassword')
+    });
   }
 
   get f() { return this.registerForm.controls; }
@@ -29,17 +35,47 @@ export class RegisterComponent implements OnInit {
     if (this.registerForm.invalid) {
       return;
     }
-
-
     this.loading = true;
     window.setTimeout(function() {
       this.loading = false;
       window.alert('registration succesful');
     }, 2000);
-
   }
-  
-  
-}
 
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+    }
+  }
+  // // Validates passwords
+  // matchPassword(group: FormGroup): any {
+  //   if (group && group.controls) {
+  //     const password = group.controls.password;
+  //     const confirm = group.controls.confirmPassword;
+  //     if (password.pristine || confirm.pristine) {
+  //         return null;
+  //     }
+  //     group.markAsTouched();
+  //     if (password.value === confirm.value) {
+  //         return null;
+  //     }
+  //     return {
+  //       invalidPassword: true
+  //     };
+  //   }
+  // }
+}
 
